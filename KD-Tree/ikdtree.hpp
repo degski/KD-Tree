f27a6a821aea49ad00c8104d2f4e6327bf82f80d
degski <degski@gmail.com>
2019-01-24 09:13:49 +0200

    save

--- KD-Tree/ikdtree.hpp
+++ KD-Tree/ikdtree.hpp
@@ -552,21 +552,23 @@ struct TreeMap2D {
 
     template<typename forward_it>
     void initialize ( forward_it first_, forward_it last_ ) noexcept {
-        if ( first_ < last_ ) {
-            const std::size_t n = std::distance ( first_, last_ );
-            if ( n > m_linear_bound ) {
-                m_data.resize ( bin_tree_size<std::size_t> ( static_cast< std::size_t > ( n ) ), std::pair<key_type, mapped_type> { key_type { std::numeric_limits<base_type>::max ( ), std::numeric_limits<base_type>::max ( ) }, mapped_type { } } );
-                m_leaf_start = m_data.data ( ) + ( m_data.size ( ) / 2 ) - 1;
-                m_dim = get_dimensions_order ( first_, last_ );
-                switch ( m_dim ) {
-                case 0: kd_construct_xy ( m_data.data ( ), first_, last_ ); break;
-                case 1: kd_construct_yx ( m_data.data ( ), first_, last_ ); break;
+        if ( m_data.empty ( ) ) {
+            if ( first_ < last_ ) {
+                const std::size_t n = std::distance ( first_, last_ );
+                if ( n > m_linear_bound ) {
+                    m_data.resize ( bin_tree_size<std::size_t> ( static_cast< std::size_t > ( n ) ), std::pair<key_type, mapped_type> { key_type { std::numeric_limits<base_type>::max ( ), std::numeric_limits<base_type>::max ( ) }, mapped_type { } } );
+                    m_leaf_start = m_data.data ( ) + ( m_data.size ( ) / 2 ) - 1;
+                    m_dim = get_dimensions_order ( first_, last_ );
+                    switch ( m_dim ) {
+                    case 0: kd_construct_xy ( m_data.data ( ), first_, last_ ); break;
+                    case 1: kd_construct_yx ( m_data.data ( ), first_, last_ ); break;
+                    }
                 }
-            }
-            else {
-                m_data.reserve ( n );
-                std::copy ( first_, last_, std::back_inserter ( m_data ) );
-                m_dim = 2u;
+                else {
+                    m_data.reserve ( n );
+                    std::copy ( first_, last_, std::back_inserter ( m_data ) );
+                    m_dim = 2u;
+                }
             }
         }
     }
